6305cbce57c1742afdc4fda2596a306494c96c2c
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-02-28 22:19:48 +0700

    feat: theming, colors, fonts

---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = (): JSX.Element => {
+  return (
+    <div className="relative h-screen bg-green_main font-bold">
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default Login;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,80 +1,38 @@
-import type { Config } from "tailwindcss"
-
-const config = {
-  darkMode: ["class"],
+module.exports = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class',
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+        white: '#FCFCFC',
+        green_main: '#48AD83',
+        grey: '#C2C9D3',
+        text_grey: '#A3A3A3',
+        green_light: '#7AC0A3',
+        green_dark: '#3D8567',
+        blue_main: '#005AAB',
+        blue_light: '#499AE3',
+        blue_dark: '#074A87',
+        black: '#1C1C1C',
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '2.5xl': '1.653rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem'
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+      // dropShadow: {
+      //   green: '0 2px 4px rgba(137, 193, 144, 0.25)'
+      // }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+  variants: {},
+  // plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
+}
