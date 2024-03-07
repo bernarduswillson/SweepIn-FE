b430d032fb2e48a55b1fb90233bdb1f24a2e7b87
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-08 00:22:25 +0700

    fix: fix attendance header styling

--- src/app/presensi/page.tsx
+++ src/app/presensi/page.tsx
@@ -41,40 +41,38 @@ const Presensi = (): JSX.Element => {
   }
 
   return (
-    <div className="relative w-screen h-screen flex justify-center bg-blue_main overflow-hidden">
-      <div className='w-11/12 max-w-[641px]'>
+    <div className="relative w-screen h-screen flex flex-col items-center bg-blue_main overflow-hidden">
 
         {/* Header */}
-        <div className='relative h-[30vh]'>
+        <div className='relative w-11/12 max-w-[641px] h-[40vh]'>
           <Navbar />
           
-          {/* <div className='justify-end flex flex-col bottom-0 absolute'>
-            <div className='mb-10 ml-[5vw]'>
+          <div className='justify-end flex flex-col bottom-0 absolute'>
+            <div className='mb-7'>
               <h1 className="text-white text-5xl pt-10 font-extrabold">Presensi</h1>
               <h2 className="text-white text-md font-semibold">{getToday()}</h2>
             </div>
           </div>
+
           <div className='absolute bottom-0 right-0 translate-x-[100px] translate-y-[50px]'>
             <Image src={Calendar} alt='Calendar' />
-          </div> */}
+          </div>
         </div>
 
         {/* Body */}
-        {/* <div className="h-[70vh] px-[5vw] w-full bg-white bottom-0 relative rounded-tl-[40px] flex flex-col">
+        <div className="w-full max-w-[641px] h-[70vh] bg-white rounded-t-[26px] flex flex-col">
           <div className='h-fit'>
             <h1 className="text-black text-2xl py-6 font-bold">Riwayat Presensi</h1>
             <SearchBar />
           </div>
           <div className='overflow-y-auto flex-1 mt-5 rounded-xl'>
             <div className='w-full h-fit gap-1'>
-              {DataPresensi.map((data, index) => (
+              {/* {DataPresensi.map((data, index) => (
                 <Card key={index} date={data.date} status={data.status} />
-              ))} 
+              ))}  */}
             </div>
           </div>
-        </div> */}
-
-      </div>
+        </div>
 
       {/* {showAlert && <AlertLog onConfirm={handleConfirm} onCancel={handleConfirm} />} */}
     
