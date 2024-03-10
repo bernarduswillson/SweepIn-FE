304797cfcda319e0272970f58a23a6a3a41fe816
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-10 17:16:09 +0700

    feat: refactor description input

--- src/app/laporan/baru/page.tsx
+++ src/app/laporan/baru/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
 
 // Components
 import FormHeader from '@/components/ui/FormHeader';
@@ -15,40 +15,47 @@ const FormLaporan = (): JSX.Element => {
   // List of photos
   const [photos, setPhotos] = useState<File[]>([]);
 
+  // Description data
+  const [desc, setDesc] = useState<string>('');
+
+  // Handle description change
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDesc(e.target.value);
+  }
+
   return (
-    <>
-      <div className="w-screen min-h-screen h-fit flex flex-col items-center gap-5 bg-gradient-to-br from-green_main to-blue_main to-[50vh]">
-      
-        {/* Head */}
-        <div className='w-11/12 max-w-[641px] py-10 flex flex-col items-center'>
-          <FormHeader title='Laporan Kerja' date={getTodayDate()} />
-          <ReportGalleryInput photos={photos} setPhotos={setPhotos} />
-        </div>
+    <div className="w-screen min-h-screen h-fit flex flex-col items-center gap-5 bg-gradient-to-br from-green_main to-blue_main to-[50vh]">
+    
+      {/* Head */}
+      <div className='w-11/12 max-w-[641px] py-10 flex flex-col items-center'>
+        <FormHeader title='Laporan Kerja' date={getTodayDate()} />
+        <ReportGalleryInput photos={photos} setPhotos={setPhotos} />
+      </div>
 
-        {/* Body */}
-        <div className="w-full max-w-[641px] flex justify-center flex-grow py-6 bg-white rounded-t-[26px]">
-          <div className='w-11/12 h-fit flex flex-col'>
-            {/* Text input */}
-            <h4 className="text-green_main text-base poppins-bold">Nama</h4>
-            <h3 className="text-black text-xl poppins-medium">Ditra Rizqa Amadia</h3>
-            <h4 className="text-green_main text-base mt-5 poppins-bold">Tanggal</h4>
-            <h3 className="text-black text-xl poppins-medium">{getTodayString()}</h3>
-            <h4 className="text-green_main text-base mt-5 poppins-bold">Deskripsi</h4>
-            <textarea 
-              rows={3} 
-              value=""
-              placeholder='Contoh: Menjaga gerbang kampus'
-              className='px-3 py-2 mt-2 bg-grey_bg poppins-medium text-black placeholder:text-grey_text' 
-            />
+      {/* Body */}
+      <div className="w-full max-w-[641px] flex justify-center flex-grow py-6 bg-white rounded-t-[26px]">
+        <div className='w-11/12 h-fit flex flex-col'>
+          {/* Text input */}
+          <h4 className="text-green_main text-base poppins-bold">Nama</h4>
+          <h3 className="text-black text-xl poppins-medium">Ditra Rizqa Amadia</h3>
+          <h4 className="text-green_main text-base mt-5 poppins-bold">Tanggal</h4>
+          <h3 className="text-black text-xl poppins-medium">{getTodayString()}</h3>
+          <h4 className="text-green_main text-base mt-5 poppins-bold">Deskripsi</h4>
+          <textarea 
+            rows={3} 
+            value={desc}
+            placeholder='Contoh: Menjaga gerbang kampus'
+            className='px-3 py-2 mt-2 bg-grey_bg poppins-medium text-black placeholder:text-grey_text' 
+            onChange={handleDescriptionChange}
+          />
 
-            {/* Submit button */}
-            <div className="flex flex-col items-center pt-10">
-              <SubmitButton text='Kirim' onClick={() => {}} loading={false} disable={false}/>
-            </div>
+          {/* Submit button */}
+          <div className="flex flex-col items-center pt-10">
+            <SubmitButton text='Kirim' onClick={() => {}} loading={false} disable={false}/>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
   
