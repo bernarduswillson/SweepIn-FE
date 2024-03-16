fcda1f4f792b0b56f6a894ddb242ce108eeb99e6
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-16 22:37:38 +0700

    style: update sidebar background color to blue

--- src/components/ui/SidebarButton.tsx
+++ src/components/ui/SidebarButton.tsx
@@ -12,8 +12,8 @@ const SidebarButton = (props: SidebarButtonProps) => {
 
   return (
     <Link href={url}>
-      <div className={`p-2 ${active ? 'bg-grey_bg' : 'bg-transparent'} rounded-md cursor-pointer transition-colors duration-150 ease-in-out hover:bg-grey_bg`}>
-        <span className='text-black poppins-medium'>{text}</span>
+      <div className={`p-2 ${active ? 'bg-blue_dark' : 'bg-transparent'} rounded-md cursor-pointer transition-colors duration-150 ease-in-out hover:bg-blue_dark`}>
+        <span className='text-white poppins-medium'>{text}</span>
       </div>
     </Link>
   );
